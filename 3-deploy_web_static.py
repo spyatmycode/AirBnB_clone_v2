d65c90535ae10a3083c456ac87bf2cc8fb946a93
spyatmycode <akejunifemi11@gmail.com>
2023-03-19 15:41:44 +0100

    New commit two

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 """
-Fabric script methods:
-    do_pack: packs web_static/ files into .tgz archive
-    do_deploy: deploys archive to webservers
-    deploy: do_packs && do_deploys
-Usage:
-    fab -f 3-deploy_web_static.py deploy -i my_ssh_private_key -u ubuntu
+Fabric script based on the file 2-do_deploy_web_static.py that creates and
+distributes an archive to the web servers
 """
-from fabric.api import local, env, put, run
+
+from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-
-env.hosts = ['34.239.147.107', '52.3.247.207']
+env.hosts = ['142.44.167.228', '144.217.246.195']
 
 
 def do_pack():
@@ -23,7 +19,7 @@ def do_pack():
         file_name = "versions/web_static_{}.tgz".format(date)
         local("tar -cvzf {} web_static".format(file_name))
         return file_name
-    except Exception:
+    except:
         return None
 
 
@@ -44,7 +40,7 @@ def do_deploy(archive_path):
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except Exception:
+    except:
         return False
 
 
